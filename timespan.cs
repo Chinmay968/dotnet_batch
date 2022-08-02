b4fbb051311098fa974d432527dac9131d1c6e97
Chinmay968 <chinmay.daund@gmail.com>
2022-08-02 17:09:34 +0530

    Changes made

--- timespan.cs
+++ timespan.cs
@@ -6,17 +6,18 @@ namespace timespan
 
          static void Main(string[] args)
          {
-            timespan.Diff.diff();
+            DateTime date1 = new DateTime (2022, 08, 5, 11, 11, 11);
+            DateTime date2 = new DateTime (2022, 08, 10, 11, 11, 11);
+            timespan.Diff.diff(date1,date2);
          }
      }
 
      class Diff{
 
-        public static void diff()
+        public static void diff(DateTime t1,DateTime t2)
         {
-            DateTime date1 = new DateTime (2022, 08, 5, 11, 11, 11);
-            DateTime date2 = new DateTime (2022, 08, 10, 11, 11, 11);
-            TimeSpan interval = date2 - date1;  
+        
+            TimeSpan interval = t2 - t1;  
             Console.WriteLine("Timespan : " + interval);  
         }
 
