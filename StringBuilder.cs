f0a0933bd6281eab21ddcab439d355c34b1cffeb
Chinmay968 <chinmay.daund@gmail.com>
2022-08-02 17:41:25 +0530

    String Builder

---
+++ StringBuilder.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Text;
+
+    namespace String
+    {
+        class String{
+
+            static void Main(string[] args){
+
+               StringBuilder addWord = new StringBuilder ("Software");
+               addWord.Append (" Developer");
+               Console.WriteLine(addWord);
+
+            }
+           
+        }
+        
+    }
+   
+
