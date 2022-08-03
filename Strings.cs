17adebf50be0f54d5b567fd11e4235ca2b63ea01
Chinmay968 <chinmay.daund@gmail.com>
2022-08-03 14:32:48 +0530

    String Operation

---
+++ Strings.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Text;
+
+    namespace StringOperation{
+        class Operations{
+            static void Main (String[] args){
+                StringOperation.concatenation.concat();
+                StringOperation.Builder.build();
+
+            }
+
+        }
+
+        class concatenation{
+            public static void concat(){
+
+                 string s1 = "Hello ";
+                 string s2 = "Myself Chinmay Daund ";
+                 string result = s1 + s2;
+                 Console.WriteLine(result);
+
+            }
+        }
+        class Builder {
+            public static void build(){
+                
+                StringBuilder addWord = new StringBuilder ("Hello ");
+                addWord.Append (" Myself Chinmay Daund");
+                Console.WriteLine(addWord);
+
+            }
+        }    
+        
+    }
